516bab6c5292da6666d5e5e6ffffdedafc6a249f
vitalii-vov <71486507+vitalii-vov@users.noreply.github.com>
2025-07-05 01:56:33 +0400

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -5,7 +5,6 @@ from pathlib import Path
 def get_visible_dirs_and_files(path: Path):
     folders = []
     files = []
-
     for item in path.iterdir():
         if item.name.startswith('.') or item.name == 'index.json':
             continue
@@ -13,32 +12,35 @@ def get_visible_dirs_and_files(path: Path):
             folders.append(item.name)
         elif item.is_file():
             files.append(item.name)
-
     return sorted(folders), sorted(files)
 
+def generate_all_folders_from_files(root_path: Path) -> set[Path]:
+    all_dirs = set()
+
+    # Идём по всем файлам, даже глубоко
+    for file in root_path.rglob('*'):
+        if file.is_file():
+            for parent in file.parents:
+                if root_path in parent.parents or parent == root_path:
+                    all_dirs.add(parent)
+    return all_dirs
+
 def generate_index(root_path: str):
-    root = Path(root_path)
+    root = Path(root_path).resolve()
+    if not root.exists():
+        print(f"Path {root} doesn't exist")
+        return
 
-    for path in root.rglob('*'):
-        if path.is_dir():
-            folders, files = get_visible_dirs_and_files(path)
-            index = {
-                "folders": folders,
-                "files": files
-            }
-            index_path = path / 'index.json'
-            with open(index_path, 'w') as f:
-                json.dump(index, f, indent=2)
+    all_dirs = generate_all_folders_from_files(root)
 
-    # Создаём index.json в корне отдельно (если его нет)
-    folders, files = get_visible_dirs_and_files(root)
-    index = {
-        "folders": folders,
-        "files": files
-    }
-    index_path = root / 'index.json'
-    with open(index_path, 'w') as f:
-        json.dump(index, f, indent=2)
+    for path in all_dirs:
+        folders, files = get_visible_dirs_and_files(path)
+        index = {
+            "folders": folders,
+            "files": files
+        }
+        index_path = path / 'index.json'
+        index_path.write_text(json.dumps(index, indent=2))
 
 if __name__ == '__main__':
     generate_index('resources')
